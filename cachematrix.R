7dfff2c6d30273329f0b85fcd6594cd42d43aea4
Andre Burati <burati@gmail.com>
2014-07-23 23:12:01 -0300

    Writing MakeChacheMatrix
    first try

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,30 @@
 ## functions do
  
 ## Write a short comment describing this function
+makeCacheMatrix <- function( m = matrix() ) {
 
-makeCacheMatrix <- function(x = matrix()) {
+	## Initialize the inverse property
+    i <- NULL
 
-}
+    ## Method to set the matrix
+    set <- function( matrix ) {
+            m <<- matrix
+            i <<- NULL
+    }
 
+    ## Method the get the matrix
+    get <- function() {
+    	## Return the matrix
+    	m
+    }
 
-## Write a short comment describing this function
+    ## Method to set the inverse of the matrix
+    setInverse <- function(inverse) {
+        i <<- inverse
+    }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+    ## Method to get the inverse of the matrix
+    getInverse <- function() {
+        ## Return the inverse property
+        i
+    }
